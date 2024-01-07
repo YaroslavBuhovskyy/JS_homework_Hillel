8210c8f3fd0377e800653589e8c87a10c1e0cd0e
YaroslavBuhovskyy <97852835+YaroslavBuhovskyy@users.noreply.github.com>
2024-01-07 21:33:46 +0200

    Update hw_6_task_2.js

--- hw_6_task_2.js
+++ hw_6_task_2.js
@@ -1,58 +1,56 @@
-const company = {
-    name: 'Велика Компанія',
-    type:'Головна компанія',
-    platform: 'Платформа для продажу квитків',
-    sellsSolution: 'Рішення для продажу квитків',
-    clients: [
-        {
-            name: 'Клієнт 1',
-            type: 'subCompany',
-            uses: 'ПО для продажу квитків',
-            sells: 'Рішення для продажу квитків',
-            partners: [
-                {
-                    name: 'Клієнт 1.1',
-                    type: 'subSubCompany',
-                    uses: 'Рішення для продажу квитків',
-                    sells: 'Рішення для продажу квитків',
-                },
-                {
-                    name: 'Клієнт 1.2',
-                    type: 'subSubCompany',
-                    uses: 'Рішення для продажу квитків',
-                    sells: 'Рішення для продажу квитків',
-                    partners: [
-                        {
-                            name: 'Клієнт 1.2.3',
-                            type: 'subSubCompany',
-                            uses: 'Рішення для продажу квитків',
-                            sells: 'Рішення для продажу квитків',
-                        }
-                    ]
-                }
-            ]
-        },
-        {
-            name: 'Клієнт 2',
-            type: 'subCompany',
-            uses: 'ПО для продажу квитків',
-            sells: 'Рішення для продажу квитків'
-        }
-    ]
-};
-
-// написати функцію findValueByKey(companyName), яка буде приймати значення у вигляді companyName та надавати інформацію про цю subCompany
-
 function findValueByKey(companyName) {
+    const company = {
+        name: 'Велика Компанія',
+        type:'Головна компанія',
+        platform: 'Платформа для продажу квитків',
+        sellsSolution: 'Рішення для продажу квитків',
+        clients: [
+            {
+                name: 'Клієнт 1',
+                type: 'subCompany',
+                uses: 'ПО для продажу квитків',
+                sells: 'Рішення для продажу квитків',
+                partners: [
+                    {
+                        name: 'Клієнт 1.1',
+                        type: 'subSubCompany',
+                        uses: 'Рішення для продажу квитків',
+                        sells: 'Рішення для продажу квитків',
+                    },
+                    {
+                        name: 'Клієнт 1.2',
+                        type: 'subSubCompany',
+                        uses: 'Рішення для продажу квитків',
+                        sells: 'Рішення для продажу квитків',
+                        partners: [
+                            {
+                                name: 'Клієнт 1.2.3',
+                                type: 'subSubCompany',
+                                uses: 'Рішення для продажу квитків',
+                                sells: 'Рішення для продажу квитків',
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                name: 'Клієнт 2',
+                type: 'subCompany',
+                uses: 'ПО для продажу квитків',
+                sells: 'Рішення для продажу квитків'
+            }
+        ]
+    };
+
     function findCompanyInfo(object, name) {
-        if (object.name === name) {
+        if (object.name.toLowerCase() === name.toLowerCase()) {
             return object;
         }
 
-        if (object.clients && Array.isArray(object.clients)) {
-            for (let client of object.clients) {
-                let result = findCompanyInfo(client, name);
-                if (result) {
+        if (object.clients) {
+            for (let i = 0; i < object.clients.length; i++) {
+                const result = findCompanyInfo(object.clients[i], name);
+                if (result !== null) {
                     return result;
                 }
             }
@@ -64,6 +62,15 @@ function findValueByKey(companyName) {
     return findCompanyInfo(company, companyName);
 }
 
-let companyInfo = findValueByKey('Клієнт 1');
-//let companyInfo = findValueByKey('Клієнт 2');
-console.log(companyInfo);
+let companyInfo1 = findValueByKey('Клієнт 1.1');
+let companyInfo2 = findValueByKey('Клієнт 1.2');
+let companyInfo3 = findValueByKey('Клієнт 1');
+let companyInfo4 = findValueByKey('Клієнт 2');
+let companyInfo5 = findValueByKey('Клієнт 2.1');
+let companyInfo6 = findValueByKey('Клієнт 3');
+console.log(companyInfo1);
+console.log(companyInfo2);
+console.log(companyInfo3);
+console.log(companyInfo4);
+console.log(companyInfo5);
+console.log(companyInfo6);
