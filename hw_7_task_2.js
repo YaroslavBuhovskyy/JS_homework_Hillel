1a498a9c1f75accaceb36cfd8820ab7c9dd1a124
Yaroslav Buhovskyy <y.buhovskyy@gmail.com>
2024-01-10 21:51:27 +0200

    hw_7_task_2_DONE

--- hw_7_task_2.js
+++ hw_7_task_2.js
@@ -1,30 +1,55 @@
-var arr = [
-    {
-        userName:"Test",
-        lastName:"Test",
-        email:"test.test@gmail.com"
-    },
-    {
-        userName:"Dmitro",
-        lastName:"Porohov",
-        email:"dmitro.porohov@yahoo.com"
-    },
-    {
-        userName:"Andrii",
-        lastName:"",
-        email:"andrii@mail.ru" // Нам такі не підходять
-    },
-];
-
 // 1) довіряємо лише gmail.com та yahoo.com
 // 2) зробити просту валідацію до @ -> одне, або два слова які можуть містити (латинські букви, цифри) + може бути розділене крапкою (yaroslav.buhovskyy@gmail.com)
 
-//let re = /^[a-zA-Z0-9.]+@(gmail|yahoo).com$/ тут без валідації
-let re = /^[a-zA-Z0-9]+\.{0,1}[a-zA-Z0-9]+@(gmail|yahoo).com$/ 
+var arr = [
+        {
+            userName:"Test",
+            lastName:"Test",
+            email:"test@gmail.com"
+        },
+            {
+            userName:"Test",
+            lastName:"Test",
+            email:"test.1@gmail.com"
+        },
+            {
+            userName:"Test",
+            lastName:"Test",
+            email:"t.e.st@yahoo.com"
+        },
+            {
+            userName:"Test",
+            lastName:"Test",
+            email:"t12@gmail.co"
+        },
+        {
+            userName:"Dmitro",
+            lastName:"Porohov",
+            email:"dmitro.porohov@yahoo.com"
+        },
+        {
+            userName:"Andrii",
+            lastName:"",
+            email:"andrii@mail.ru" 
+        },
+        {
+            userName:"Andrii",
+            lastName:"",
+            email:"1@yahoo.com" 
+        },
+    ];
 
-arr.forEach(obj => {
-    console.log(re.test(obj.email));
-});
+const re = /^[a-zA-Z0-9]+\.{0,1}[a-zA-Z0-9]+@(gmail|yahoo).com$/
 
-// let re = /^[a-zA-Z0-9.{0,1}]+@(gmail|yahoo).com$/ цей скоріше тавтологічний?
+const result = arr.filter((item) => re.test(item.email));
+console.log(result);
 
+//Фільтрація масиву
+/*var arr = [2, 8, 4, 12, 34, 3, 51];
+var res = arr.filter(checker);
+console.log("arr=" + arr);
+console.log("res=" + res);
+
+function checker(item) {
+    return item > 8;
+}*/
